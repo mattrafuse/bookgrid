1083b6916f567fae989d8fb53810c346ffbf8eab
Matthew Rafuse <15745691+mattrafuse@users.noreply.github.com>
2024-01-07 18:29:03 -0500

    UI prep complete

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
-import './App.scss';
-import { Container } from '@mui/material';
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Container></Container>;
+  return (
+    <>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h4">Book Grid</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ py: 2, flex: 1 }}>
+        <Typography variant="h4">Hello</Typography>
+      </Container>
+    </>
+  );
 }
 
 export default App;
