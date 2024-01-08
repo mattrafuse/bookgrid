c09cd38003d3d1cb7a34cfb3a154c651c9fca666
Matthew Rafuse <15745691+mattrafuse@users.noreply.github.com>
2024-01-07 22:50:25 -0500

    lots of stuff

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,115 @@
 import { useState } from 'react';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Stack,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  alpha,
+} from '@mui/material';
+
+import Refresh from '@mui/icons-material/Refresh';
+import { brown } from '@mui/material/colors';
+
+type TabsType = 'today' | 'yesterday' | 'archived';
+
+const TABS: TabsType[] = ['today', 'yesterday', 'archived'];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [tab, setTab] = useState<TabsType>('today');
 
   return (
     <>
       <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h4">Book Grid</Typography>
+        <Toolbar
+          sx={{
+            backgroundColor: brown[700],
+          }}
+        >
+          <Stack justifyContent="space-between" direction="row" width="100%">
+            <Typography variant="h4">Book Grid</Typography>
+            <Tabs
+              value={tab}
+              onChange={(_ev, value) => setTab(value)}
+              sx={theme => ({
+                '& button': {
+                  color: alpha(theme.palette.common.white, 0.6),
+                  textTransform: 'capitalize',
+                  fontSize: '1.15rem',
+                  '&.Mui-selected': {
+                    color: theme.palette.common.white,
+                  },
+                },
+                '& .MuiTabs-indicator': {
+                  backgroundColor: theme.palette.common.white,
+                },
+              })}
+            >
+              {TABS.map(_tab => (
+                <Tab value={_tab} label={_tab} />
+              ))}
+            </Tabs>
+            <Box>
+              <IconButton>
+                <Refresh />
+              </IconButton>
+            </Box>
+          </Stack>
         </Toolbar>
       </AppBar>
       <Container sx={{ py: 2, flex: 1 }}>
         <Typography variant="h4">Hello</Typography>
+        <Typography variant="body1">Explanation stuff</Typography>
+        <Box
+          sx={theme => ({
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            gridAutoRows: '1fr',
+            width: '600px',
+            height: '600px',
+            border: '3px solid black',
+            borderRadius: theme.shape.borderRadius,
+            overflow: 'hidden',
+            '& > div': {
+              backgroundColor: 'grey',
+              '&:not(:nth-child(3n))': {
+                borderRight: 'thin solid black',
+              },
+              '&:nth-child(-n+6)': {
+                borderBottom: 'thin solid black',
+              },
+            },
+          })}
+        >
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </Box>
       </Container>
+      <footer>
+        <Stack
+          spacing={1}
+          justifyContent="center"
+          sx={{ textAlign: 'center', p: 1 }}
+        >
+          <Typography variant="caption" color="text.secondary">
+            Created By: R.J. Arsenault, Alex Kerzner and Matthew Rafuse
+          </Typography>
+          <Typography variant="caption" color="text.secondary">
+            Copyright 2024
+          </Typography>
+        </Stack>
+      </footer>
     </>
   );
 }
