2d9453b7269e6fff6c18fe256d494d0948460b69
Matthew Rafuse <15745691+mattrafuse@users.noreply.github.com>
2024-01-20 20:56:02 -0500

    Set up backend endpoint

--- server/server.py
+++ server/server.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<p>Home</p>"
+@app.route("/check_guess", methods=["POST"])
+def check_guess():
+    """
+    Check whether the given guess is valid. Expects a JSON post request in the format:
+
+    {
+        "guess": "Some book title"
+    }
+
+    and returns True iff the guess field equals War and Peace.
+
+    TODO: Make it not a static value that is correct
+    """
+    data = request.json
+
+    return jsonify({
+        "valid": data["guess"] == "War and Peace"
+    })
